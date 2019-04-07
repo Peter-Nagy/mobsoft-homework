c3ed9131f098fc554eb28f65bd519a1f2a7d4d11
Peter Nagy <nagypeterszabolcs@gmail.com>
2019-04-07 21:37:12 +0200

    Add new activity to ApplicationComponent

--- app/src/main/java/com/peter/nagy/mobsoft/homework/MoviesApplicationComponent.kt
+++ app/src/main/java/com/peter/nagy/mobsoft/homework/MoviesApplicationComponent.kt
@@ -3,12 +3,14 @@ package com.peter.nagy.mobsoft.homework
 import com.peter.nagy.mobsoft.homework.interactor.InteractorModule
 import com.peter.nagy.mobsoft.homework.network.NetworkModule
 import com.peter.nagy.mobsoft.homework.ui.UIModule
-import com.peter.nagy.mobsoft.homework.ui.main.MainActivity
+import com.peter.nagy.mobsoft.homework.ui.movieDetails.MovieDetailsActivity
+import com.peter.nagy.mobsoft.homework.ui.movies.MoviesActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [UIModule::class, NetworkModule::class, InteractorModule::class])
 interface MoviesApplicationComponent {
-    fun inject(mainActivity: MainActivity)
+    fun inject(moviesActivity: MoviesActivity)
+    fun inject(movieDetailsActivity: MovieDetailsActivity)
 }

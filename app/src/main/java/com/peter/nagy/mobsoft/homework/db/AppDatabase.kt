106d5f4930132937d4a38cd984eb44a537fac388
Peter Nagy <nagypeterszabolcs@gmail.com>
2019-04-28 16:24:35 +0200

    Add room

---
+++ app/src/main/java/com/peter/nagy/mobsoft/homework/db/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.peter.nagy.mobsoft.homework.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = [Like::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun likeDao(): LikeDao
+
+    companion object {
+        var INSTANCE: AppDatabase? = null
+
+        fun getAppDataBase(context: Context): AppDatabase? {
+            if (INSTANCE == null){
+                synchronized(AppDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "myDB").build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyDataBase(){
+            INSTANCE = null
+        }
+    }
+}
---
+++ app/src/main/java/com/peter/nagy/mobsoft/homework/db/Like.kt
@@ -0,0 +1,28 @@
+package com.peter.nagy.mobsoft.homework.db
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.Query
+
+@Entity
+data class Like(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val movieId: Int)
+
+@Dao
+interface LikeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertLike(like: Like)
+
+    @Delete
+    fun deleteLike(like: Like)
+
+    @Query("SELECT * FROM `LIKE` WHERE movieId == :movieId")
+    fun getLike(movieId: Int): LiveData<Like>
+}
